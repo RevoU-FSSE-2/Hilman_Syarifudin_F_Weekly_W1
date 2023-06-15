354e730f3a85b2ceaa179abb4881b5c7f14484af
Hill-Man-1 <hilmaaansf@gmail.com>
2023-06-15 14:38:06 +0700

    add JS internal JS external still error

--- Hilman Syarifudin F_Weekly_W1/JS/index.js
+++ Hilman Syarifudin F_Weekly_W1/JS/index.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    let person = prompt("Hello. Input Your Name");
-    if (person !== "") {
-      alert("Hello " + person + "! Thanks For Coming To My Website😁")
-    } else {
-      alert("You Put The Wrong Value. Refresh and Try Again!")
-      }
+  let person = prompt("Hello. Input Your Name");
+  if (person !== "") {
+    alert("Hello " + person + "! Thanks For Coming To My Website😁")
+  } else {
+    alert("You Put The Wrong Value. Refresh and Try Again!")
   }
+}
